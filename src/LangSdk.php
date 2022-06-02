f232b42699694813e72fa217f855a31904111c14
ngy <ray.nip@topkee.com.hk>
2022-06-02 14:08:49 +0800

    使用说明

--- src/LangSdk.php
+++ src/LangSdk.php
@@ -308,7 +308,7 @@ class LangSdk
         }
         return $rs;
     }
-    public function decodeUnicode($str)
+    public static function decodeUnicode($str)
     {
         return preg_replace_callback('/\\\\u([0-9a-f]{4})/i', function($matches){return mb_convert_encoding(pack("H*", $matches[1]), "UTF-8", "UCS-2BE");}, $str);
     }
